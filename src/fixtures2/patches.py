34780f4c259aad9a371ca8398984cc2b8e335edd
CooledCoffee <cooledcoffee@gmail.com>
2014-05-26 15:18:17 +0800

    minor change

--- src/fixtures2/patches.py
+++ src/fixtures2/patches.py
@@ -3,6 +3,6 @@ from fixtures._fixtures.monkeypatch import MonkeyPatch
 from fixtures.fixture import Fixture
 
 class PatchesFixture(Fixture):
-    def patch(self, path, value):
-        self.useFixture(MonkeyPatch(path, value))
+    def patch(self, path, value=None):
+        self.useFixture(MonkeyPatch(path, new_value=value))
         
